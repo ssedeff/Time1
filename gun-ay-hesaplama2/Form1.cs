d1596a1dded46ca4ea9ded6ed60a71c8b834b88d
ssedeff <145463803+ssedeff@users.noreply.github.com>
2024-01-09 14:11:12 +0300

    Add files via upload

---
+++ gun-ay-hesaplama2/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace gun_ay_hesaplama2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+         
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+            sayac++;
+        }
+        int sayac = 0;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            sayac++;
+            label1.Text=sayac.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            sayac = 0;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+
+            timer1.Stop();
+        }
+    }
+}
